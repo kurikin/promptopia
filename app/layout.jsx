3ae5f7f4aa7fd6f9ad7a7d9c99c0dfb0a49f3621
kurikin <riku.code0309@gmail.com>
2023-07-21 11:46:07 +0900

    Add texts to home page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,20 @@
 import "@styles/globals.css";
-import { Children } from "react";
 
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
 };
 
-const RootLayout = () => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
         <div className="main">
           <div className="gradient" />
         </div>
-      </body>
 
-      <main className="app">{Children}</main>
+        <main className="app">{children}</main>
+      </body>
     </html>
   );
 };
