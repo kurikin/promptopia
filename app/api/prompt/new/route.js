3114445dccf68ff8ee82f16300556351430b9cf7
kurikin <riku.code0309@gmail.com>
2023-07-22 07:27:25 +0900

    Fix import of page.jsx

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -11,5 +11,7 @@ export const POST = async (req) => {
     await newPrompt.save();
 
     return new Response(JSON.stringify(newPrompt), { status: 201 });
-  } catch (error) {}
+  } catch (error) {
+    return new Response("Failed to create a new prompt", { status: 500 });
+  }
 };
