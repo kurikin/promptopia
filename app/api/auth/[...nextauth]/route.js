40774bc78afa08477850ec29752c6f14dafca9f8
kurikin <riku.code0309@gmail.com>
2023-07-21 15:22:30 +0900

    Update model for user

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectToDB } from "@utils/database";
+
 const handler = NextAuth({
   providers: GoogleProvider({
     clientId: process.env.GOOGLE_ID,
@@ -9,7 +11,17 @@ const handler = NextAuth({
   async session({ session }) {},
   async signIn({ profile }) {
     try {
-    } catch (error) {}
+      await connectToDB();
+
+      // Check a user already exists
+
+      // if not, create a new user
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 });
 
